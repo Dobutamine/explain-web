bba284a62d58f607e951b64c439d14ee608414c7
dobutamine <tajantonius@gmail.com>
2024-05-10 00:14:39 +0200

    ecls model

--- src/explain/device_models/Ecls.js
+++ src/explain/device_models/Ecls.js
@@ -1,11 +1,3 @@
-import {
-  BloodCapacitance,
-  BloodPump,
-  BloodResistor,
-  GasCapacitance,
-  GasExchanger,
-  GasResistor,
-} from "../ModelIndex";
 import { set_gas_composition } from "../helpers/GasComposition";
 
 export class Ecls {
@@ -119,84 +111,83 @@ export class Ecls {
 
   build_tubing_in() {
     // define a blood capacitance which represents the tubing on the inlet side
-    this._tubing_in = new BloodCapacitance(
-      this._model_engine,
-      "_tubing_in",
-      "BloodCapacitance"
-    );
+    this._tubing_in = this._model_engine.models["ECLS_TUBIN"];
+
     // we need to calculate the unstressed volume of the tubing depending on the length and diameter
     let tubing_in_uvol = this.calc_volume(
       this.tubing_in_length,
       this.tubing_diameter
     );
+
     this._tubing_in.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: false },
       { key: "vol", value: tubing_in_uvol },
       { key: "u_vol", value: tubing_in_uvol },
       { key: "el_base", value: this.tubing_elastance },
     ]);
+
     // set the electrolytes
     this._tubing_in.solutes = { ...this._model_engine.models["AA"].solutes };
     this._tubing_in.aboxy = { ...this._model_engine.models["AA"].aboxy };
+
     // calculate the starting pressure
     this._tubing_in.calc_model();
+
     // add the tubing in to the ecls parts list
     this._ecls_parts.push(this._tubing_in);
   }
   build_tubing_out() {
     // define a blood capacitance which represents the tubing on the inlet side
-    this._tubing_out = new BloodCapacitance(
-      this._model_engine,
-      "_tubing_out",
-      "BloodCapacitance"
-    );
+    this._tubing_out = this._model_engine.models["ECLS_TUBOUT"];
     // we need to calculate the unstressed volume of the tubing depending on the length and diameter
     let tubing_out_uvol = this.calc_volume(
       this.tubing_out_length,
       this.tubing_diameter
     );
     this._tubing_out.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: false },
       { key: "vol", value: tubing_out_uvol },
       { key: "u_vol", value: tubing_out_uvol },
       { key: "el_base", value: this.tubing_elastance },
     ]);
+
     // set the electrolytes
     this._tubing_out.solutes = { ...this._model_engine.models["AA"].solutes };
     this._tubing_out.aboxy = { ...this._model_engine.models["AA"].aboxy };
+
     // calculate the starting pressure
     this._tubing_out.calc_model();
+
     // add the tubing in to the ecls parts list
     this._ecls_parts.push(this._tubing_out);
   }
   build_oxy() {
     // define a blood capacitance which represents the tubing on the inlet side
-    this._oxy = new BloodCapacitance(
-      this._model_engine,
-      "_oxy",
-      "BloodCapacitance"
-    );
+    this._oxy = this._model_engine.models["ECLS_OXY"];
     this._oxy.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: false },
       { key: "vol", value: this.oxy_volume },
       { key: "u_vol", value: this.oxy_volume },
       { key: "el_base", value: 10000 },
     ]);
+
     // set the electrolytes
     this._oxy.solutes = { ...this._model_engine.models["AA"].solutes };
     this._oxy.aboxy = { ...this._model_engine.models["AA"].aboxy };
+
     // calculate the starting pressure
     this._oxy.calc_model();
     // add the tubing in to the ecls parts list
     this._ecls_parts.push(this._oxy);
   }
+
   init_pump() {
     // define a blood capacitance which represents the tubing on the inlet side
     this._pump.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: false },
       { key: "vol", value: this.pump_volume },
       { key: "u_vol", value: this.pump_volume },
@@ -213,25 +204,23 @@ export class Ecls {
     // add the tubing in to the ecls parts list
     this._ecls_parts.push(this._pump);
   }
+
   build_blood_part() {
     this.build_tubing_in();
     this.build_tubing_out();
     this.build_oxy();
+
     // define a blood pump but don't initialize it because the connectors have not been built yet
-    this._pump = new BloodPump(this._model_engine, "_pump", "BloodPump");
+    this._pump = this._model_engine.models["ECLS_PUMP"];
+
     // calculate the resistance of this cannula depending on the length and diameter
     let drainage_res = this.inlet_res;
     let return_res = this.outlet_res;
 
     // connect the parts
-    this._drainage_cannula = new BloodResistor(
-      this._model_engine,
-      "_drainage_cannula",
-      "BloodResistor"
-    );
-
+    this._drainage_cannula = this._model_engine.models["ECLS_DR"];
     this._drainage_cannula.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       {
@@ -243,16 +232,13 @@ export class Ecls {
       { key: "r_back", value: drainage_res },
       { key: "r_k", value: 0.0 },
     ]);
+
     // add the resistor to the list
     this._ecls_parts.push(this._drainage_cannula);
 
-    this._tubing_in_pump = new BloodResistor(
-      this._model_engine,
-      "_tubing_in_pump",
-      "BloodResistor"
-    );
+    this._tubing_in_pump = this._model_engine.models["ECLS_TUBIN_PUMP"];
     this._tubing_in_pump.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       { key: "comp_from", value: this._tubing_in },
@@ -261,16 +247,13 @@ export class Ecls {
       { key: "r_back", value: 50.0 },
       { key: "r_k", value: 0.0 },
     ]);
+
     // add the resistor to the list
     this._ecls_parts.push(this._tubing_in_pump);
 
-    this._pump_oxy = new BloodResistor(
-      this._model_engine,
-      "_pump_oxy",
-      "BloodResistor"
-    );
+    this._pump_oxy = this._model_engine.models["ECLS_PUMP_OXY"];
     this._pump_oxy.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       { key: "comp_from", value: this._pump },
@@ -279,16 +262,13 @@ export class Ecls {
       { key: "r_back", value: 50.0 },
       { key: "r_k", value: 0.0 },
     ]);
+
     // add the resistor to the list
     this._ecls_parts.push(this._pump_oxy);
 
-    this._oxy_tubing_out = new BloodResistor(
-      this._model_engine,
-      "_oxy_tubing_out",
-      "BloodResistor"
-    );
+    this._oxy_tubing_out = this._model_engine.models["ECLS_OXY_TUBOUT"];
     this._oxy_tubing_out.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       { key: "comp_from", value: this._oxy },
@@ -297,18 +277,14 @@ export class Ecls {
       { key: "r_back", value: 50.0 },
       { key: "r_k", value: 0.0 },
     ]);
+
     // add the resistor to the list
     this._ecls_parts.push(this._oxy_tubing_out);
 
-    this._return_cannula = new BloodResistor(
-      this._model_engine,
-      "_return_cannula",
-      "BloodResistor"
-    );
-    // calculate the resistance of this cannula depending on the length and diameter
+    this._return_cannula = this._model_engine.models["ECLS_RE"];
 
     this._return_cannula.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       { key: "comp_from", value: this._tubing_out },
@@ -317,20 +293,18 @@ export class Ecls {
       { key: "r_back", value: return_res },
       { key: "r_k", value: 0.0 },
     ]);
+
     // add the resistor to the list
     this._ecls_parts.push(this._return_cannula);
 
     // initialize the pump
     this.init_pump();
   }
+
   build_gas_part() {
-    this._gas_in = new GasCapacitance(
-      this._model_engine,
-      "_gas_in",
-      "GasCapacitance"
-    );
+    this._gas_in = this._model_engine.models["ECLS_GASIN"];
     this._gas_in.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: true },
       { key: "vol", value: 5.4 },
       { key: "u_vol", value: 5.0 },
@@ -350,13 +324,9 @@ export class Ecls {
     // add to the vent parts array
     this._ecls_parts.push(this._gas_in);
 
-    this._gas_oxy = new GasCapacitance(
-      this._model_engine,
-      "_gas_oxy",
-      "GasCapacitance"
-    );
+    this._gas_oxy = this._model_engine.models["ECLS_GASOXY"];
     this._gas_oxy.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: false },
       { key: "vol", value: 0.1 },
       { key: "u_vol", value: 0.1 },
@@ -376,13 +346,9 @@ export class Ecls {
     // add to the vent parts array
     this._ecls_parts.push(this._gas_oxy);
 
-    this._gas_out = new GasCapacitance(
-      this._model_engine,
-      "_gas_out",
-      "GasCapacitance"
-    );
+    this._gas_out = this._model_engine.models["ECLS_GASOUT"];
     this._gas_out.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "fixed_composition", value: true },
       { key: "vol", value: 5.0 },
       { key: "u_vol", value: 5.0 },
@@ -398,13 +364,9 @@ export class Ecls {
     this._ecls_parts.push(this._gas_out);
 
     // connect the parts
-    this._gas_in_oxy = new GasResistor(
-      this._model_engine,
-      "_gas_in_oxy",
-      "GasResistor"
-    );
+    this._gas_in_oxy = this._model_engine.models["ECLS_GASIN_OXY"];
     this._gas_in_oxy.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       { key: "comp_from", value: this._gas_in },
@@ -415,13 +377,9 @@ export class Ecls {
     ]);
     this._ecls_parts.push(this._gas_in_oxy);
 
-    this._gas_oxy_out = new GasResistor(
-      this._model_engine,
-      "_gas_oxy_out",
-      "GasResistor"
-    );
+    this._gas_oxy_out = this._model_engine.models["ECLS_OXY_GASOUT"];
     this._gas_oxy_out.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "no_flow", value: false },
       { key: "no_back_flow", value: false },
       { key: "comp_from", value: this._gas_oxy },
@@ -432,14 +390,11 @@ export class Ecls {
     ]);
     this._ecls_parts.push(this._gas_oxy_out);
   }
+
   build_gasexchanger() {
-    this._gasex = new GasExchanger(
-      this._model_engine,
-      "_gasex",
-      "GasExchanger"
-    );
+    this._gasex = this._model_engine.models["ECLS_GASEX"];
     this._gasex.init_model([
-      { key: "is_enabled", value: true },
+      { key: "is_enabled", value: false },
       { key: "comp_blood", value: this._oxy },
       { key: "comp_gas", value: this._gas_oxy },
       { key: "dif_o2", value: this.diff_o2 },
