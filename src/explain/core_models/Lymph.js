d273cdb79ef6da63bc1a6c87fb46805ac003b8a4
dobutamine <tajantonius@gmail.com>
2024-05-14 19:20:09 +0200

    lymfatics

--- src/explain/core_models/Lymph.js
+++ src/explain/core_models/Lymph.js
@@ -7,36 +7,13 @@ export class Lymph {
   description = "";
   is_enabled = false;
   dependencies = [];
-  ducts = ["LD"];
-  trunks = ["LT"];
-  interstitium = ["IS"];
-  pump = [];
-  starlings = [
-    "INT_IS",
-    "KID_IS",
-    "RLB_IS",
-    "LS_IS",
-    "RUB_IS",
-    "LL_IS",
-    "RL_IS",
-  ];
 
   // dependent parameters
-  is_flow = 0.0;
-  is_lt_flow = 0.0;
-  lt_ld_flow = 0.0;
-  ld_svc_flow = 0.0;
 
   // local parameters
   _model_engine = {};
   _is_initialized = false;
   _t = 0.0005;
-  _analysis_counter = 0.0;
-  _analysis_interval = 2.0;
-  _cum_is_flow = 0.0;
-  _cum_is_lt_flow = 0.0;
-  _cum_lt_ld_flow = 0.0;
-  _cum_ld_svc_flow = 0.0;
 
   // the constructor builds a bare bone modelobject of the correct type and with the correct name and stores a reference to the modelengine object
   constructor(model_ref, name = "", type = "") {
@@ -59,39 +36,8 @@ export class Lymph {
     // set the modeling step size
     this._t = this._model_engine.modeling_stepsize;
 
-    // set the aboxy and solutes
-    for (let [model_name, model] of Object.entries(this._model_engine.models)) {
-      if (model.model_type === "LymphCapacitance") {
-        if (Object.keys(model.aboxy).length < 1) {
-          model.aboxy = { ...this.aboxy };
-          model.solutes = { ...this.solutes };
-        }
-      }
-    }
     // set the flag to model is initialized
     this._is_initialized = true;
-  }
-
-  switch_lymphatics(state) {
-    this.interstitium.forEach((i) => {
-      this._model_engine.models[i].is_enabled = state;
-    });
-    this.trunks.forEach((i) => {
-      this._model_engine.models[i].is_enabled = state;
-    });
-    this.ducts.forEach((i) => {
-      this._model_engine.models[i].is_enabled = state;
-    });
-    this.pump.forEach((i) => {
-      this._model_engine.models[i].is_enabled = state;
-    });
-    this.starlings.forEach((i) => {
-      this._model_engine.models[i].is_enabled = state;
-    });
-    this.is_enabled = state;
-
-    //this._model_engine.rebuildExecutionList = true;
-    console.log(this._model_engine);
   }
   step_model() {
     if (this.is_enabled && this._is_initialized) {
@@ -99,28 +45,5 @@ export class Lymph {
     }
   }
 
-  calc_model() {
-    if (this._analysis_counter > this._analysis_interval) {
-      this.is_flow = (this._cum_is_flow / this._analysis_counter) * 60.0;
-      this.is_lt_flow = (this._cum_is_lt_flow / this._analysis_counter) * 60.0;
-      this.lt_ld_flow = (this._cum_lt_ld_flow / this._analysis_counter) * 60.0;
-      this.ld_svc_flow =
-        (this._cum_ld_svc_flow / this._analysis_counter) * 60.0;
-
-      this._cum_is_flow = 0.0;
-      this._cum_is_lt_flow = 0.0;
-      this._cum_lt_ld_flow = 0.0;
-      this._cum_ld_svc_flow = 0.0;
-
-      this._analysis_counter = 0.0;
-    }
-    this.starlings.forEach((cap) => {
-      this._cum_is_flow += this._model_engine.models[cap].flow * this._t;
-    });
-    this._cum_is_lt_flow += this._model_engine.models["IS_LT"].flow * this._t;
-    this._cum_lt_ld_flow += this._model_engine.models["LT_LD"].flow * this._t;
-    this._cum_ld_svc_flow += this._model_engine.models["LD_SVC"].flow * this._t;
-
-    this._analysis_counter += this._t;
-  }
+  calc_model() {}
 }
