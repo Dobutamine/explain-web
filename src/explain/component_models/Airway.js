b529ed1953bf960918d2b5c6d59a89d21f46c59a
dobutamine <timantonius@pm.me>
2025-04-18 16:13:15 +0200

    update

--- src/explain/component_models/Airway.js
+++ src/explain/component_models/Airway.js
@@ -4,18 +4,4 @@ export class Airway extends GasCapacitance {
   // static properties
   static model_type = "Airway";
 
-
-  constructor(model_ref, name = "") {
-    super(model_ref, name);
-  }
-
-  calc_model() {
-    this.calc_resistances();
-    this.calc_elastances();
-    this.calc_volumes();
-    this.calc_pressure();
-  }
-  calc_resistances() {
-
-  }
 }
